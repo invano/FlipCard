20c930ef7b5478414434b6482f548f821887048f
xit4 <v.labalestra@gmail.com>
2014-05-17 01:57:48 +0200

    HandSelection1

--- FlipCard-WP/Page1.xaml.cs
+++ FlipCard-WP/Page1.xaml.cs
@@ -12,19 +12,34 @@ using System.Windows.Input;
 
 namespace FlipCard_WP
 {
+
+    public struct CardInfo
+    {
+        public string name;
+        public Image img;
+        public string source;
+        public bool isUp;
+    }
+
     public partial class Page1 : PhoneApplicationPage
     {
         private TranslateTransform dragTranslation;
         private TranslateTransform originalTranslation;
-    
+        private CardInfo[] cards = new CardInfo[8];
 
         public Page1()
         {
-      
-            InitializeComponent();
-            Card1.Source = new ImageSourceConverter().ConvertFromString("/Assets/ImagesCards/Card1Blue.png") as ImageSource;
 
-            
+            InitializeComponent();
+            cards[0].img = Card0;
+            cards[0].name = "Card0";
+            cards[1].img = Card1;
+            cards[1].name = "Card1";
+
+            Card0.Source = new ImageSourceConverter().ConvertFromString("/Assets/ImagesCards/Card1Blue.png") as ImageSource;
+            Card1.Source = new ImageSourceConverter().ConvertFromString("/Assets/ImagesCards/Card1Red.png") as ImageSource;
+
+
             //Card1.ManipulationDelta += new EventHandler<ManipulationDeltaEventArgs>(Drag_ManipulationDelta);
             //Card1.ManipulationCompleted += new EventHandler<ManipulationCompletedEventArgs>(Drag_ManipulationCompleted);
             //Card1.ManipulationStarted += new EventHandler<ManipulationStartedEventArgs>(Drag_ManipulationStarted);
@@ -32,22 +47,22 @@ namespace FlipCard_WP
             //dragTranslation = new TranslateTransform();
             //originalTranslation = new TranslateTransform();
 
-           
 
 
-            
+
+
         }
 
         private void MouseMoving(object sender, MouseEventArgs e)
         {
-            if (sender.GetType() == typeof(Image))
-            {
-                Image realSender = (Image)sender;
-                Canvas.SetZIndex(realSender, 32);
-            }
+            //if (sender.GetType() == typeof(Image))
+            //{
+            //    Image realSender = (Image)sender;
+            //    Canvas.SetZIndex(realSender, 32);
+            //}
         }
 
-        
+
         private void Drag_ManipulationStarted(object sender, ManipulationStartedEventArgs e)
         {
             dragTranslation = new TranslateTransform();
@@ -65,7 +80,7 @@ namespace FlipCard_WP
 
 
             MessageBox.Show("Dude " + (originalTranslation.X + dragTranslation.X));
-            
+
             dragTranslation.X = originalTranslation.X;
             dragTranslation.Y = originalTranslation.Y;
         }
@@ -76,6 +91,42 @@ namespace FlipCard_WP
             dragTranslation.X += e.DeltaManipulation.Translation.X;
             dragTranslation.Y += e.DeltaManipulation.Translation.Y;
         }
-       
+
+        private void Card_Tap(object sender, GestureEventArgs e)
+        {
+            Image img = (Image)sender;
+            TranslateTransform translation = new TranslateTransform();
+            for (int i = 0; i < 7; i++)
+            {
+                if (img.Name.Equals(cards[i].name))
+                {
+                    if (cards[i].isUp)
+                    {
+                        img.RenderTransform = translation;
+                        translation.Y += 0;
+                        cards[i].isUp = false;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < 7; j++)
+                        {
+                            if (cards[j].isUp)
+                            {
+                                Image img2 = cards[j].img;
+                                TranslateTransform translation2 = new TranslateTransform();
+                                img2.RenderTransform = translation2;
+                                translation2.Y += 0;
+                                cards[j].isUp = false;
+                            }
+                        }
+                            img.RenderTransform = translation;
+                        translation.Y -= 20;
+                        cards[i].isUp = true;
+                    }
+                }
+            }
+
+        }
+
     }
 }
