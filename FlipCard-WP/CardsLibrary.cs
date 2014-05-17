953476e3b07286bcdc4943477848962538896cc2
Fabriziodenny90 <fabriziodenny@gmail.com>
2014-05-17 14:06:47 +0200

    random funziona

--- FlipCard-WP/CardsLibrary.cs
+++ FlipCard-WP/CardsLibrary.cs
@@ -12,6 +12,7 @@ namespace FlipCard_WP
         public Card[] CommonCardsInLibrary;
         public Card[] SilverCardsInLibrary;
         public Card[] GoldCardsInLibrary;
+        Random rgn = new Random();
 
 
         public CardsLibrary()
@@ -362,23 +363,26 @@ namespace FlipCard_WP
             Deck myRandomDeck = new Deck();
     
             int i = 0;
-            Random rgn = new Random();
+            
             int goldNumber = (intsize / Const.GOLD_RARITY_PROBABILITY_DIVIDER);
             for (i = 0; i < goldNumber; i++) {
-                int casualGoldPosition = rgn.Next() % this.GoldCardsInLibrary.Count();
+                int casualGoldPosition = rgn.Next(0, this.GoldCardsInLibrary.Count());
+               // int casualGoldPosition = rgn.Next() % this.GoldCardsInLibrary.Count();
                 myRandomDeck.addCardToDeck(this.GoldCardsInLibrary[casualGoldPosition]);
             }
 
             int silverNumber = (intsize / Const.SILVER_RARITY_PROBABILITY_DIVIDER);
             for (i = 0; i < silverNumber; i++) {
-                int casualSilverPosition = rgn.Next() % this.SilverCardsInLibrary.Count();
+                int casualSilverPosition = rgn.Next(0, this.SilverCardsInLibrary.Count());
+               // int casualSilverPosition = rgn.Next() % this.SilverCardsInLibrary.Count();
                 myRandomDeck.addCardToDeck(this.SilverCardsInLibrary[casualSilverPosition]);
 
             }
     
             int commonNumber = intsize - goldNumber - silverNumber;
             for (i=0; i<commonNumber; i++) {
-                int casualCommonPosition = rgn.Next() % this.CommonCardsInLibrary.Count();
+                int casualCommonPosition = rgn.Next(0, this.CommonCardsInLibrary.Count());
+                //int casualCommonPosition = rgn.Next() % this.CommonCardsInLibrary.Count();
                 myRandomDeck.addCardToDeck(this.CommonCardsInLibrary[casualCommonPosition]);
             }
             return myRandomDeck;
