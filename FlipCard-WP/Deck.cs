562e99a2f26e47db01a9006626c61289f4a3b5ff
Fabriziodenny90 <fabriziodenny@gmail.com>
2014-05-17 02:40:41 +0200

    deck1

--- FlipCard-WP/Deck.cs
+++ FlipCard-WP/Deck.cs
@@ -8,5 +8,43 @@ namespace FlipCard_WP
 {
     class Deck
     {
+        int no_cards;
+        public Card[] cardArray;
+
+
+        public void setDeck(Card[] deck ) {
+
+            deck.CopyTo(this.cardArray, this.no_cards);
+        }
+
+        public Card[] getDeck() {
+            return this.cardArray;
+                }
+        
+        public void set_no_cards(int no_cards) {
+            this.no_cards = no_cards;
+        }
+
+        public int get_no_cards() {
+            return this.no_cards;
+        }
+
+        public Card getRandomCard() {
+            int i = 0;
+            if (this.no_cards != 0)
+            {
+                i = new Random().Next(0, this.no_cards + 1);
+                return this.cardArray[i];
+            }
+            return null;
+
+        }
+        public void shuffle() {
+
+
+
+
+        }
+
     }
 }
